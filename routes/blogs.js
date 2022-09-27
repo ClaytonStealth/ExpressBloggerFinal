b7ba002e949e3141a3b7db1fc6a740f975aa5b36
claytonstealth <claytonses@yahoo.com>
2022-09-27 12:56:30 -0400

    get-one route added and working correctly

--- routes/blogs.js
+++ routes/blogs.js
@@ -9,15 +9,45 @@ const {
 } = require("../mongo")
 
 router.get('/get-one-example', async function (req, res, next) {
-    const blogPost = await db().collection("posts").findOne({
-        id: {
-            $exists: true
-        }
-    })
-    res.json({
-        success: true,
-        post: blogPost
-    })
+    try {
+        const blogPost = await db().collection("posts").findOne({
+            id: {
+                $exists: true
+            }
+        })
+        res.json({
+            success: true,
+            post: blogPost
+        })
+    } catch (err) {
+        //In the catch block, we always want to do 2 things: console.log the error and respond with an error object
+        console.log(err.name)
+        res.json({
+            success: false,
+            error: err.toString()
+        })
+    }
+});
+
+router.get('/get-one/:id', async function (req, res, next) {
+    try {
+        const blogId = req.params.id
+        const blogPost = await db().collection("posts").findOne({
+            id: blogId
+        })
+        res.json({
+            success: true,
+            post: blogPost
+        })
+    } catch (err) {
+        //In the catch block, we always want to do 2 things: console.log the error and respond with an error object
+        console.log(err.name)
+        res.json({
+            success: false,
+            error: err.toString()
+        })
+    }
 });
 
 module.exports = router;
+// try {} catch () {}
